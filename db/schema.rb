1b1d0d20ef5c373e82a1d81afbb8134a54dc4a0f
Tun Amsyar <tun.amsyar@gmail.com>
2016-10-04 17:19:28 +0800

    Added playlist controller. Able to search songs.

--- db/schema.rb
+++ db/schema.rb
@@ -10,18 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
 ActiveRecord::Schema.define(version: 20160928080117) do
-=======
-<<<<<<< HEAD
-ActiveRecord::Schema.define(version: 20160928043642) do
-=======
-
-ActiveRecord::Schema.define(version: 20160927051048) do
-
-
->>>>>>> master
->>>>>>> a193215005b6d42e8a14da37cc6c0d6a211c173e
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,16 +28,6 @@ ActiveRecord::Schema.define(version: 20160927051048) do
 
   create_table "users", force: :cascade do |t|
     t.string   "email"
-<<<<<<< HEAD
-    t.string   "password"
-    t.date     "date_of_birth"
-    t.string   "name"
-    t.datetime "created_at",           null: false
-    t.datetime "updated_at",           null: false
-    t.string   "password_digest"
-    t.string   "password_reset_token"
-    t.datetime "password_reset_at"
-=======
     t.string   "username"
     t.string   "password_digest"
     t.string   "password_reset_token"
@@ -62,7 +41,6 @@ ActiveRecord::Schema.define(version: 20160927051048) do
     t.integer  "role",                 default: 0
     t.datetime "created_at",                       null: false
     t.datetime "updated_at",                       null: false
->>>>>>> master
   end
 
 end
