a9a53d741dbd6de8268a62d0a07ba46d320f91d3
Tun Amsyar <tun.amsyar@gmail.com>
2016-10-05 10:13:59 +0800

    small changes to playlist controller, playlist index, playlist/song_search.html

--- app/controllers/playlist_controller.rb
+++ app/controllers/playlist_controller.rb
@@ -10,64 +10,68 @@ class PlaylistController < ApplicationController
 
     if @song == []
 
-     m = Date.parse(date).mon
-     y = Date.parse(date).year
-
-     case m
-      when 1
-        m = y
-        date = y.to_s + ("-01-14")
-        @song = Song.search(date)
-      when 2
-        m = y
-        date = y.to_s + ("-01-14")
-        @song = Song.search(date)
-      when 3
-        m = y
-        date = y.to_s + ("-01-14")
-        @song = Song.search(date)
-      when 4
-        m = y
-        date = y.to_s + ("-05-14")
-        @song = Song.search(date)
-      when 5
-        m = y
-        date = y.to_s + ("-05-14")
-        @song = Song.search(date)
-      when 6
-        m = y
-        date = y.to_s + ("-05-14")
-        @song = Song.search(date)
-      when 7
-        m = y
-        date = y.to_s + ("-05-14")
-        @song = Song.search(date)
-      when 8
-        m = y
-        date = y.to_s + ("-09-14")
-        @song = Song.search(date)
-      when 9
-        m = y
-        date = y.to_s + ("-09-14")
-        @song = Song.search(date)
-      when 10
-        m = y
-        date = y.to_s + ("-09-14")
-        @song = Song.search(date)
-      when 11
-        m = y
-        date = y.to_s + ("-09-14")
-        @song = Song.search(date)
-      when 12
-        m = y
-        date = y.to_s + ("-09-14")
-        @song = Song.search(date)
-        binding.pry
-      end
+     date_find(date)
+     binding.pry
     end
   end
 
   def view
+  end
+
+  def date_find(date)
+    m = Date.parse(date).mon
+    y = Date.parse(date).year
+
+    case m
+     when 1
+       m = y
+       date = y.to_s + ("-01-14")
+       @song = Song.search(date)
+     when 2
+       m = y
+       date = y.to_s + ("-01-14")
+       @song = Song.search(date)
+     when 3
+       m = y
+       date = y.to_s + ("-01-14")
+       @song = Song.search(date)
+     when 4
+       m = y
+       date = y.to_s + ("-05-14")
+       @song = Song.search(date)
+     when 5
+       m = y
+       date = y.to_s + ("-05-14")
+       @song = Song.search(date)
+     when 6
+       m = y
+       date = y.to_s + ("-05-14")
+       @song = Song.search(date)
+     when 7
+       m = y
+       date = y.to_s + ("-05-14")
+       @song = Song.search(date)
+     when 8
+       m = y
+       date = y.to_s + ("-09-14")
+       @song = Song.search(date)
+     when 9
+       m = y
+       date = y.to_s + ("-09-14")
+       @song = Song.search(date)
+     when 10
+       m = y
+       date = y.to_s + ("-09-14")
+       @song = Song.search(date)
+     when 11
+       m = y
+       date = y.to_s + ("-09-14")
+       @song = Song.search(date)
+     when 12
+       m = y
+       date = y.to_s + ("-09-14")
+       @song = Song.search(date)
+     end
   end
 
 
