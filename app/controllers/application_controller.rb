56e2386205edddebb55df4f4dc352bf1b19220e7
shahminrazali <shahminrazali@gmail.com>
2016-10-07 16:39:19 +0800

    done playlist

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   helper_method :current_playlist
 
-  private
-
   def current_user
     return unless session[:id]
      @current_user ||= User.find_by(id: session[:id])
@@ -19,9 +17,10 @@ class ApplicationController < ActionController::Base
 
   include CurrentUserHelper
 
-  def authenticate!
+  private
+    def authenticate!
     unless current_user
       redirect_to root_path
-    end
+
   end
 end
--- app/controllers/playlist_controller.rb
+++ app/controllers/playlist_controller.rb
@@ -2,6 +2,13 @@ class PlaylistController < ApplicationController
 
   def index
     @playlist = Playlist.new
+    @user_playlist = Playlist.all
+    @playlist_items = PlaylistItem.where(playlist_id: @user_playlist.first.id)
+    @songs = Song.where(id: @playlist_items.each {|p| p.id})
+  end
+
+  def select_playlist
+    @playlist_items = PlaylistItem.where(playlist_id: Playlist.find(params[playlist_id]))
   end
 
   def new
